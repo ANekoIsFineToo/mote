75f466178e9d4371fec95c71be4ed20fcd97380b
Iker Pérez Brunelli <yo@ikerperez.es>
2017-06-02 17:54:43 +0200

    Basic configuration

---
+++ src/containers/App/App.js
@@ -0,0 +1,16 @@
+import React, { PureComponent } from 'react';
+
+import './App.css';
+
+class App extends PureComponent {
+
+  render() {
+    return (
+      <div className="App">
+        App
+      </div>
+    );
+  }
+}
+
+export default App;
