bd42ba8407e1394060ee434534fd6cbc39f48dac
Iker Pérez Brunelli <yo@ikerperez.es>
2017-06-05 19:08:37 +0200

    Move note edit logic to a new component

---
+++ src/components/EditNote/index.js
@@ -0,0 +1,81 @@
+import React, { PureComponent } from 'react';
+import ImmutablePropTypes from 'react-immutable-proptypes';
+import { Button, Col, Container, Input, Row } from 'reactstrap';
+import PropTypes from 'prop-types';
+
+import NoteInput from '../NoteInput';
+import NoteOutput from '../NoteOutput';
+
+class EditNote extends PureComponent {
+  static propTypes = {
+    draft: ImmutablePropTypes.map.isRequired,
+    saveDraft: PropTypes.func.isRequired,
+    saveNote: PropTypes.func.isRequired,
+  };
+
+  constructor(props) {
+    super(props);
+
+    this.saveNote = this.saveNote.bind(this);
+    this.clearAll = this.clearAll.bind(this);
+    this.handleTitleChange = this.handleTitleChange.bind(this);
+    this.handleContentChange = this.handleContentChange.bind(this);
+  }
+
+  saveNote() {
+    this.props.saveNote(this.props.draft);
+  }
+
+  clearAll() {
+    const clearDraft = this.props.draft
+      .set('title', '')
+      .set('content', '');
+
+    this.props.saveDraft(clearDraft);
+  }
+
+  handleTitleChange(e) {
+    this.props.saveDraft(this.props.draft.set('title', e.target.value));
+  }
+
+  handleContentChange(content) {
+    this.props.saveDraft(this.props.draft.set('content', content));
+  }
+
+  render() {
+    return (
+      <div className="EditNote">
+        <Container>
+          <Row>
+            <Col xs="9">
+              <Input
+                className="border-0"
+                value={this.props.draft.get('title')}
+                placeholder="Nota sin título."
+                onChange={this.handleTitleChange} />
+            </Col>
+
+            <Col xs="3">
+              <Button className="mr-2" color="primary" onClick={this.saveNote}>Guardar</Button>
+              <Button outline color="warning" onClick={this.clearAll}>Vaciar</Button>
+            </Col>
+          </Row>
+        </Container>
+
+        <Container fluid>
+          <Row>
+            <Col xs="6">
+              <NoteInput onChange={this.handleContentChange} value={this.props.draft.get('content')} />
+            </Col>
+
+            <Col xs="6">
+              <NoteOutput markdown={this.props.draft.get('content')} />
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    );
+  }
+}
+
+export default EditNote;
--- src/containers/NoteAdd/index.js
+++ src/containers/NoteAdd/index.js
@@ -3,12 +3,10 @@ import { Helmet } from 'react-helmet';
 import ImmutablePropTypes from 'react-immutable-proptypes';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
-import { Button, Col, Container, Input, Row } from 'reactstrap';
 import PropTypes from 'prop-types';
 
 import * as note from '../../actions/note';
-import NoteInput from '../../components/NoteInput';
-import NoteOutput from '../../components/NoteOutput';
+import EditNote from '../../components/EditNote';
 import * as fromRoot from '../../reducers';
 
 class NoteAdd extends PureComponent {
@@ -19,37 +17,8 @@ class NoteAdd extends PureComponent {
     saveNewNote: PropTypes.func.isRequired,
   };
 
-  constructor(props) {
-    super(props);
-
-    this.saveNote = this.saveNote.bind(this);
-    this.clearAll = this.clearAll.bind(this);
-    this.handleTitleChange = this.handleTitleChange.bind(this);
-    this.handleContentChange = this.handleContentChange.bind(this);
-  }
-
   componentDidMount() {
     this.props.loadDraft(0);
-  }
-
-  saveNote() {
-    this.props.saveNewNote(this.props.draft);
-  }
-
-  clearAll() {
-    const clearDraft = this.props.draft
-      .set('title', '')
-      .set('content', '');
-
-    this.props.saveDraft(clearDraft);
-  }
-
-  handleTitleChange(e) {
-    this.props.saveDraft(this.props.draft.set('title', e.target.value));
-  }
-
-  handleContentChange(content) {
-    this.props.saveDraft(this.props.draft.set('content', content));
   }
 
   render() {
@@ -59,34 +28,7 @@ class NoteAdd extends PureComponent {
           <title>Añadir nota</title>
         </Helmet>
 
-        <Container className="mb-3">
-          <Row>
-            <Col xs="9">
-              <Input
-                className="border-0"
-                value={this.props.draft.get('title')}
-                placeholder="Nota sin título."
-                onChange={this.handleTitleChange} />
-            </Col>
-
-            <Col xs="3">
-              <Button className="mr-2" color="primary" onClick={this.saveNote}>Guardar</Button>
-              <Button outline color="warning" onClick={this.clearAll}>Vaciar</Button>
-            </Col>
-          </Row>
-        </Container>
-
-        <Container fluid>
-          <Row>
-            <Col xs="6">
-              <NoteInput onChange={this.handleContentChange} value={this.props.draft.get('content')} />
-            </Col>
-
-            <Col xs="6">
-              <NoteOutput markdown={this.props.draft.get('content')} />
-            </Col>
-          </Row>
-        </Container>
+        <EditNote draft={this.props.draft} saveDraft={this.props.saveDraft} saveNote={this.props.saveNewNote} />
       </div>
     );
   }
