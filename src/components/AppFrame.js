9da1f756a4a0a03a89296e21e2b034debfd7ffa1
Iker Pérez Brunelli <yo@ikerperez.es>
2017-06-07 20:47:33 +0200

    Add AppBar

--- src/components/AppFrame.js
+++ src/components/AppFrame.js
@@ -1,12 +1,25 @@
 import React, { PureComponent } from 'react';
+import AppBar from 'material-ui/AppBar';
+import ToolBar from 'material-ui/Toolbar';
+import Typography from 'material-ui/Typography';
 import { withStyles, createStyleSheet } from 'material-ui/styles';
+import PropTypes from 'prop-types';
 
 class AppFrame extends PureComponent {
+  static propTypes = {
+    classes: PropTypes.object.isRequired,
+  };
 
   render() {
     return (
-      <div>
-        AppFrame
+      <div className={this.props.classes.appFrame}>
+        <AppBar>
+          <ToolBar>
+            <Typography type="title" colorInherit noWrap>
+              Mote
+            </Typography>
+          </ToolBar>
+        </AppBar>
       </div>
     );
   }
@@ -29,6 +42,12 @@ const styleSheet = createStyleSheet('AppFrame', theme => ({
       WebkitFontSmoothing: 'antialiased',
       MozOsxFontSmoothing: 'grayscale',
     },
+  },
+  appFrame: {
+    display: 'flex',
+    alignItems: 'stretch',
+    minHeight: '100vh',
+    width: '100%',
   },
 }));
 
